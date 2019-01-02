08db69a58bca1f16c14ac694815f042f8d9a7b49
Vince Gerald C. dela Cerna <Vince Gerald C. dela Cerna>
2019-01-02 22:54:27 +0800

    added login for shop

--- app/src/main/java/vincegeralddelaccerna/ezwheels/LoginShop.java
+++ app/src/main/java/vincegeralddelaccerna/ezwheels/LoginShop.java
@@ -5,10 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginShop extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnShopreg, btnShophome;
+    TextView btnShopreg, btnShophome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
