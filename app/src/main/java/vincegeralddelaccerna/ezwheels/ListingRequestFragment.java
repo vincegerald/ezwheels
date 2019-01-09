3ce4d908d6ccb00306110bdebd8c9eb752b514e0
Vince Gerald C. dela Cerna <vincedelacerna123@gmail.com>
2019-01-09 16:19:31 +0800

    jan 9 commit

--- app/src/main/java/vincegeralddelaccerna/ezwheels/ListingRequestFragment.java
+++ app/src/main/java/vincegeralddelaccerna/ezwheels/ListingRequestFragment.java
@@ -9,19 +9,81 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 public class ListingRequestFragment extends Fragment implements View.OnClickListener {
 
+    RecyclerView recyclerView;
     FloatingActionButton add;
+    DatabaseReference databaseReference;
+    FirebaseAuth mAuth;
+    FirebaseRecyclerOptions<Upload> options;
+    FirebaseRecyclerAdapter<Upload, ListRequestHolder> adapter;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =  inflater.inflate(R.layout.listingrequest_fragment, null);
+        recyclerView = v.findViewById(R.id.recyclerRequest);
+        recyclerView.setHasFixedSize(true);
+
+        mAuth = FirebaseAuth.getInstance();
+        String uid = mAuth.getUid().toString();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child(uid).child("Listing Request");
         add = v.findViewById(R.id.fab);
         add.setOnClickListener(this);
+
+        options = new FirebaseRecyclerOptions.Builder<Upload>()
+                .setQuery(databaseReference, Upload.class).build();
+
+        adapter = new FirebaseRecyclerAdapter<Upload, ListRequestHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ListRequestHolder holder, int position, @NonNull Upload model) {
+
+                Picasso.get().load(model.getImagePath1()).into(holder.image1, new Callback() {
+                    @Override
+                    public void onSuccess() {
+
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }   
+                });
+
+                holder.name.setText(model.getFinalBrand());
+                holder.price.setText(model.getFinalPrice());
+                holder.d1.setText(model.getFinalColor());
+                holder.d2.setText(model.getFinalMileage());
+                holder.d3.setText(model.getFinalTransmission());
+                holder.d4.setText(model.getFinalYear());
+                holder.s1.setText(model.getFinalPrice());
+
+
+            }
+
+            @NonNull
+            @Override
+            public ListRequestHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent,false);
+
+                return new ListRequestHolder(view);
+            }
+        };
+        recyclerView.setAdapter(adapter);
         return v;
     }
 
@@ -41,5 +103,30 @@ public class ListingRequestFragment extends Fragment implements View.OnClickList
                     .replace(R.id.screen_area, listing, listing.getTag())
                     .commit();
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if(adapter != null){
+            adapter.startListening();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(adapter != null){
+            adapter.startListening();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if(adapter != null){
+            adapter.startListening();
+        }
+        super.onStop();
     }
 }
--- app/src/main/java/vincegeralddelaccerna/ezwheels/Upload.java
+++ app/src/main/java/vincegeralddelaccerna/ezwheels/Upload.java
@@ -23,4 +23,124 @@ public class Upload{
         this.shop = shop;
         this.status = status;
     }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getImagePath1() {
+        return imagePath1;
+    }
+
+    public void setImagePath1(String imagePath1) {
+        this.imagePath1 = imagePath1;
+    }
+
+    public String getImagePath2() {
+        return imagePath2;
+    }
+
+    public void setImagePath2(String imagePath2) {
+        this.imagePath2 = imagePath2;
+    }
+
+    public String getImagePath3() {
+        return imagePath3;
+    }
+
+    public void setImagePath3(String imagePath3) {
+        this.imagePath3 = imagePath3;
+    }
+
+    public String getVideoPath() {
+        return videoPath;
+    }
+
+    public void setVideoPath(String videoPath) {
+        this.videoPath = videoPath;
+    }
+
+    public String getFinalBrand() {
+        return finalBrand;
+    }
+
+    public void setFinalBrand(String finalBrand) {
+        this.finalBrand = finalBrand;
+    }
+
+    public String getFinalModel() {
+        return finalModel;
+    }
+
+    public void setFinalModel(String finalModel) {
+        this.finalModel = finalModel;
+    }
+
+    public String getFinalYear() {
+        return finalYear;
+    }
+
+    public void setFinalYear(String finalYear) {
+        this.finalYear = finalYear;
+    }
+
+    public String getFinalColor() {
+        return finalColor;
+    }
+
+    public void setFinalColor(String finalColor) {
+        this.finalColor = finalColor;
+    }
+
+    public String getFinalTransmission() {
+        return finalTransmission;
+    }
+
+    public void setFinalTransmission(String finalTransmission) {
+        this.finalTransmission = finalTransmission;
+    }
+
+    public String getFinalPcondition() {
+        return finalPcondition;
+    }
+
+    public void setFinalPcondition(String finalPcondition) {
+        this.finalPcondition = finalPcondition;
+    }
+
+    public String getFinalMileage() {
+        return finalMileage;
+    }
+
+    public void setFinalMileage(String finalMileage) {
+        this.finalMileage = finalMileage;
+    }
+
+    public String getFinalPrice() {
+        return finalPrice;
+    }
+
+    public void setFinalPrice(String finalPrice) {
+        this.finalPrice = finalPrice;
+    }
+
+    public String getShop() {
+        return shop;
+    }
+
+    public void setShop(String shop) {
+        this.shop = shop;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
