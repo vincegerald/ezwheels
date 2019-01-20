5dba10ef67eafcbbfd647d2ac9256e81555a9c5e
Vince Gerald C. dela Cerna <vincedelacerna123@gmail.com>
2019-01-21 05:59:39 +0800

    jan 21 commit

--- app/src/main/java/vincegeralddelaccerna/ezwheels/Reservation.java
+++ app/src/main/java/vincegeralddelaccerna/ezwheels/Reservation.java
@@ -1,12 +1,62 @@
 package vincegeralddelaccerna.ezwheels;
 
-class Reservation {
+public class Reservation {
+    public String addressText, reminderText, shopuid,  currentDate,  currentTime, uid;
 
-    public Reservation() {
+    public String getAddressText() {
+        return addressText;
+    }
+
+    public void setAddressText(String addressText) {
+        this.addressText = addressText;
+    }
+
+    public String getReminderText() {
+        return reminderText;
+    }
+
+    public void setReminderText(String reminderText) {
+        this.reminderText = reminderText;
+    }
+
+    public String getShopuid() {
+        return shopuid;
+    }
+
+    public void setShopuid(String shopuid) {
+        this.shopuid = shopuid;
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(String currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(String currentTime) {
+        this.currentTime = currentTime;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
     }
 
     public Reservation(String addressText, String reminderText, String shopuid, String currentDate, String currentTime, String uid) {
+        this.addressText = addressText;
+        this.reminderText = reminderText;
+        this.shopuid = shopuid;
+        this.currentDate = currentDate;
+        this.currentTime = currentTime;
+        this.uid = uid;
     }
-
-
 }
