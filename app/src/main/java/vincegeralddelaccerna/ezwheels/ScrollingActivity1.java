223313620f11bb450eefe9c42ba3e8dda401d698
unknown <vincedelacerna123@gmail.coom>
2019-02-05 05:57:59 +0800

    ratings and reports create payment and notif

--- app/src/main/java/vincegeralddelaccerna/ezwheels/ScrollingActivity1.java
+++ app/src/main/java/vincegeralddelaccerna/ezwheels/ScrollingActivity1.java
@@ -457,7 +457,7 @@ public class ScrollingActivity1 extends AppCompatActivity implements View.OnClic
             });
 
             AlertDialog alertDialog = builder.create();
-            alertDialog.setTitle("Mark " + brand + " " + model + " as sold?");
+            alertDialog.setTitle(brand + " " + model +);
             alertDialog.show();
 
 //
